4a1ede9e77ed9388bb4a3b0b76794d28f743ec42
Guilherme <gui.barros.js@gmail.com>
2023-05-10 20:41:26 -0300

    feat: caesar cipher decode

--- script.js
+++ script.js
@@ -1,39 +1,57 @@
-window.onload = () => {
-  const form = $('form')
-  const textToEncode = $('.text-to-encode')
-  const resultSection = $('.result-section')
-  const textResult = $('.text-result')
-
-  form.onsubmit = (event) => {
-    event.preventDefault()
-  
-    const encodedText = ceasarCipherTextEncrypt(textToEncode.value)
-
-    resetElements()
-
-    resultSection.classList.remove('hide')
-    textResult.value = encodedText
-  }
-
-  function resetElements() {
-    textToEncode.value = ''
-    textResult.value = ''
-  }
-}
-
-const $ = (query) => {
+const $ = (query, options = {
+  all: false,
+  relativeElement: null
+}) => {
   if (!query) {
     return document
   }
 
-  return document.querySelector(query)
-}
-
-function ceasarCipherTextEncrypt(text, offset = 3) {
-  if (offset < 0) {
-    offset = 0
+  if (options.all) {
+    return options.relativeElement?.querySelector(query) ?? document.querySelectorAll(query)
   }
 
+  return options.relativeElement?.querySelector(query) ?? document.querySelector(query)
+}
+
+window.onload = () => {
+  const forms = $('form', {
+    all: true
+  })
+
+  forms.forEach(form => {
+    form.onsubmit = (event) => {
+      event.preventDefault()
+
+      const targetText = $('.target-text', {
+        relativeElement: form,
+      })
+
+      const targetOffset = $('.target-offset', {
+        relativeElement: form,
+      })
+
+      const targetTextValue = targetText.value.toLowerCase()
+      const targetOffsetValue = Number(targetOffset.value)
+  
+      const resultText = form.parentElement.classList.contains('encoder')
+        ? caesarCipherTextEncode(targetTextValue, targetOffsetValue)
+        : caesarCipherTextDecode(targetTextValue, targetOffsetValue)
+
+      const resultSection = $('.result-section', {
+        relativeElement: form.parentElement,
+      })
+
+      const resultTextElement = $('.result-text', {
+        relativeElement: resultSection,
+      })
+
+      resultSection.classList.remove('hide')
+      resultTextElement.value = resultText
+    }
+  })
+}
+
+function caesarCipherTextEncode(text, offset = 3) {
   const maxCharCode = 123 // z
   const minCharCode = 97 // a
   const differenceBetweenCharCodeBoundaries = maxCharCode - minCharCode
@@ -45,6 +63,28 @@ function ceasarCipherTextEncrypt(text, offset = 3) {
 
     if (charCodeOffset >= maxCharCode) {
       charCodeOffset -= differenceBetweenCharCodeBoundaries
+    }
+
+    const charOffset = String.fromCharCode(charCodeOffset)
+
+    cipherText += char === ' ' ? ' ' : charOffset
+  }
+
+  return cipherText
+}
+
+function caesarCipherTextDecode(text, offset = 3) {
+  const maxCharCode = 123 // z
+  const minCharCode = 97 // a
+  const differenceBetweenCharCodeBoundaries = maxCharCode - minCharCode
+
+  let cipherText = ''
+
+  for (const char of text) {
+    let charCodeOffset = char.charCodeAt() - offset
+
+    if (charCodeOffset < minCharCode) {
+      charCodeOffset += differenceBetweenCharCodeBoundaries
     }
 
     const charOffset = String.fromCharCode(charCodeOffset)
